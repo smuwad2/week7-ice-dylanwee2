f356df394d3ffc723017e2a4524afab8ad36c164
Dylan <70007384+dylanwee2@users.noreply.github.com>
2025-10-21 01:49:27 +0000

    Exercise 2

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -1,18 +1,35 @@
 <script>
-    export default { 
-        // Add code in appropriate places
-        data() {
-            return {
-                items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter']
-            }
-            
+    export default {
+    data() {
+        return {
+        items: ['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'],
+        newItem: '' // to store what user types in input
         }
+    },
+    methods: {
+        addItem() {
+        if (this.newItem.trim() !== '') {
+            this.items.push(this.newItem.trim())
+            this.newItem = '' // clear input after adding
+        }
+        },
+        deleteItem(index) {
+        this.items.splice(index, 1)
+        }
+    }
     }
 </script>
 
 <template>
+  <h2>Shopping Cart</h2>
 
-    <h2>Shopping Cart</h2>
-    <!-- TODO: Add Code Here -->
-   
+  <ul>
+    <li v-for="(item, index) in items" :key="index">
+      {{ item }}
+      <button @click="deleteItem(index)">Delete</button>
+    </li>
+  </ul>
+
+  <input v-model="newItem" type="text" placeholder="Add new item" />
+  <button @click="addItem">Add!</button>
 </template>
