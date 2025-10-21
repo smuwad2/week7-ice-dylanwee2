6c9fd0b873e7d47aa75405969ea6f9a612aae488
Dylan <70007384+dylanwee2@users.noreply.github.com>
2025-10-21 01:09:58 +0000

    Exercise 1

--- src/components/Ex1.vue
+++ src/components/Ex1.vue
@@ -2,6 +2,43 @@
     export default { 
         // Add Code Here to complete the task
         // Note: DO NOT USE "eval()". In security, "eval" is considered "evil"!!!
+
+        data(){
+            return {
+                x: 0,
+                y: 0,
+                selectedOp: '+',
+                operators: [
+                    '+', '-', '*', '/', '%'
+                ]
+            }
+        },
+        computed: {
+            result() {
+                const a = this.x
+                const b = this.y
+                const op = this.selectedOp
+
+                if (op === '+'){
+                    return a + b
+                }
+                else if (op === '-'){
+                    return a - b
+                }
+                else if (op === '*'){
+                    return a * b
+                }
+                else if (op === '/'){
+                    return b !== 0 ? a / b : 'Error'
+                }
+                else if (op === '%'){
+                    return b !== 0 ? a % b : 'Error'
+                }
+                else{
+                    return 'Invalid operator'
+                }
+            }
+        }
     }
 </script>
 
